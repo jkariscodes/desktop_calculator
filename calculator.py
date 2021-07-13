fb3f7f18592a3d727bf4463278a779f9beba2e05
Joseph Kariuki <joehene@gmail.com>
2021-07-14 00:44:16 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -9,9 +9,7 @@ email                : joehene@gmail.com
 /***************************************************************************
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
+ *   it under the terms of the stated license; or any later version.       *                             *
  *                                                                         *
  ***************************************************************************/
 """
